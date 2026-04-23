126a8be13e48c7f669c7725135924e00847e1251
w-bentley <wbentley@hamilton.edu>
2026-04-23 11:44:53 -0400

    Update small2.go
    Small 2 done perhaps

--- small2.go
+++ small2.go
@@ -1,20 +1,21 @@
 package main
-import ("fmt")
+import ("fmt"
+		"slices"
+)
 
 
-func reverse_slice(my_slice string) {
+func reverse_slice(my_slice []string) {
+
+for i, j :=0, len(my_slice) -1; i < j; i, j = i + 1, j - 1 {
+	my_slice[i], my_slice[j] = my_slice[j], my_slice[i]
+}
 
 }
 
-
-
-
-
-
 func main() {
+	// Declare the array to be sliced from and the first slice
 	my_array := [7]string{"I", "am", "a", "slice", "in", "an", "array"}
 	start_slice := my_array[0:4]
-
 
 	fmt.Println("The entire array contains: ", my_array)
 
@@ -25,5 +26,17 @@ func main() {
 	// An example of size determination slicing in the middle:
 	middle_slice := my_array[2:4]
 	fmt.Println("The slice of the array starting at zero contains:", middle_slice, "and has a capacity of", cap(middle_slice), "with an occupied length of", len(middle_slice))
+
+	slices.Reverse(middle_slice)
+	fmt.Println("Slice reversed via inbuilt function:", middle_slice)
+
+	// Slices also have many inbuilts, which make some code non necessary, like this reversing function I just wrote:
+	reverse_slice(middle_slice)
+	fmt.Println("Re-reversed slice using looped statement:", middle_slice)
+	
+	// Since a slice is basically a pointer list to the main array, it actually edits the main array! Lets see what happens before and after reversing...
+	fmt.Println("Before reversal:", my_array, "\n")
+	reverse_slice(start_slice)
+	fmt.Println("After reversal:", my_array, "\n")
 
 }
