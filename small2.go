c8f8cfb96deab72c4f2bf01fe1dad99808d966e8
w-bentley <wbentley@hamilton.edu>
2026-05-05 14:17:54 -0400

    Update small2.go
    comment updates for clarity

--- small2.go
+++ small2.go
@@ -13,11 +13,9 @@ import ("fmt"
 
 
 func reverse_slice(my_slice []string) {
-
 	for i, j :=0, len(my_slice) -1; i < j; i, j = i + 1, j - 1 {
 		my_slice[i], my_slice[j] = my_slice[j], my_slice[i]
 	}
-
 }
 
 
@@ -30,14 +28,14 @@ func main() {
 	fmt.Println("The entire array contains: ", my_array)
 
 	// Slice size in go is determined by the starting index and the size of whatever it is slicing
-	// So, the slice starting at index of 0 has a size of 7, but a size of 4.
+	// So, the slice starting at index of 0 has a capacity of 7, but a size of 4.
 	fmt.Println("The slice of the array starting at zero contains:", start_slice, "and has a capacity of", cap(start_slice), "with an occupied length of", len(start_slice))
 
 	// An example of size determination slicing in the middle:
 	middle_slice := my_array[2:4]
 	fmt.Println("The slice of the array starting at zero contains:", middle_slice, "and has a capacity of", cap(middle_slice), "with an occupied length of", len(middle_slice))
 
-	// Slices also have many inbuilts, which make some code non necessary, like this reversing function I just wrote:
+	// Slices also have many inbuilts, which make some code non-necessary, like this reversing function I just wrote:
 	slices.Reverse(middle_slice)
 	fmt.Println("Slice reversed via inbuilt function:", middle_slice)
 	reverse_slice(middle_slice)
